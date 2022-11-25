b1c86c0979d058ba46588ad77a2ef895fb83f7fe
shyam <shyamvelu2001@gmail.com>
2022-11-25 10:30:55 +0530

    commit

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppController {
+	
+	@GetMapping
+	public String getHome() {
+		return "home";
+	}
 
 }
